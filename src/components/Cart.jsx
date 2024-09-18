f929665b6cd382e7184d9ba891fd3a533e3af8d9
Lauro Ware <lauro.ware@gmail.com>
2024-09-18 01:13:11 -0300

    lauro

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -129,7 +129,7 @@ const Cart = () => {
                 Vaciar Carrito
               </Button>
             </Link>
-            {total > 80000 && (
+            {total > 90000 && (
               <Link to={"/checkout"}>
                 <Button
                   colorScheme="green"
@@ -139,9 +139,9 @@ const Cart = () => {
                 </Button>
               </Link>
             )}
-            {total <= 80000 && (
+            {total <= 90000 && (
               <Button colorScheme="green" onClick={() => showSolicitarAlert()}>
-                Faltan ${80000 - total} para solicitar
+                Faltan ${90000 - total} para solicitar
               </Button>
             )}
 
